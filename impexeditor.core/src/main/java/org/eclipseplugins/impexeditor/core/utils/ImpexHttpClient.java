08d88750ae9b0753a99f0367b269eec74a0bd82f
Youssef el jaoujat <eljoujat@gmail.com>
2017-08-03 10:18:40 +0200

    quality refacto

--- impexeditor.core/src/main/java/org/eclipseplugins/impexeditor/core/utils/ImpexHttpClient.java
+++ impexeditor.core/src/main/java/org/eclipseplugins/impexeditor/core/utils/ImpexHttpClient.java
@@ -188,16 +188,13 @@ public class ImpexHttpClient {
 			// set up a TrustManager that trusts everything
 			sslContext.init(null, new TrustManager[] { new X509TrustManager() {
 				public X509Certificate[] getAcceptedIssuers() {
-					System.out.println("getAcceptedIssuers =============");
 					return null;
 				}
 
 				public void checkClientTrusted(X509Certificate[] certs, String authType) {
-					System.out.println("checkClientTrusted =============");
 				}
 
 				public void checkServerTrusted(X509Certificate[] certs, String authType) {
-					System.out.println("checkServerTrusted =============");
 				}
 			} }, new SecureRandom());
 
